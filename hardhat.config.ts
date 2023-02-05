b3ec25043063d930424967fde2f05037d0e1e3d5
Jatin Jain <jatinj615@gmail.com>
2023-02-05 19:06:03 +0530

    fix: project config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,132 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-waffle";
+import { HardhatUserConfig } from 'hardhat/config';
+import "hardhat-gas-reporter";
+import "hardhat-deploy";
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-etherscan";
+import { resolve } from "path";
+import { config as dotenvConfig } from "dotenv";
+import { NetworkUserConfig } from "hardhat/types";
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+dotenvConfig({ path: resolve(__dirname, "./.env") });
+
+const chainIds = {
+  ganache: 1337,
+  goerli: 5,
+  hardhat: 1,
+  kovan: 42,
+  mainnet: 1,
+  rinkeby: 4,
+  ropsten: 3,
+};
+
+
+// Ensure that we have all the environment variables we need.
+let mnemonic: string;
+if (!process.env.MNEMONIC) {
+  throw new Error("Please set your MNEMONIC in a .env file");
+} else {
+  mnemonic = process.env.MNEMONIC;
+}
+
+let alchemyApiKey: string;
+if (!process.env.ALCHEMY_API_KEY) {
+  throw new Error("Please set your ALCHEMY_API_KEY in a .env file");
+} else {
+  alchemyApiKey = process.env.ALCHEMY_API_KEY;
+}
+
+function createTestnetConfig(network: keyof typeof chainIds): NetworkUserConfig {
+  const url: string = "https://eth-" + network + ".alchemyapi.io/v2/" + alchemyApiKey;
+  return {
+    accounts: {
+      count: 10,
+      initialIndex: 0,
+      mnemonic,
+      path: "m/44'/60'/0'/0",
+    },
+    chainId: chainIds[network],
+    url,
+  };
+}
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.0",
+  defaultNetwork: "hardhat",
+  gasReporter: {
+    currency: "USD",
+    enabled: process.env.REPORT_GAS ? true : false,
+    excludeContracts: [],
+    src: "./contracts",
+  },
+  networks: {
+    localhost: {
+      live: false,
+      saveDeployments: true,
+      tags: ["local"],
+    },
+    hardhat: {
+      // accounts: {
+      //   mnemonic,
+      // },
+      chainId: chainIds.hardhat,
+      live: false,
+      saveDeployments: true,
+      tags: ["test", "local"],
+      forking: {
+        url: "https://eth-mainnet.alchemyapi.io/v2/" + alchemyApiKey
+      }
+    },
+    goerli: createTestnetConfig("goerli"),
+    kovan: createTestnetConfig("kovan"),
+    rinkeby: createTestnetConfig("rinkeby"),
+    ropsten: createTestnetConfig("ropsten"),
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+    },
+    feeCollector: {
+      default: 1, // here this will by default take the second account as feeCollector (so in the test this will be a different account than the deployer)
+    },
+  },
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+        settings: {
+          metadata: {
+            // Not including the metadata hash
+            bytecodeHash: "none",
+          },
+          optimizer: {
+            enabled: true,
+            runs: 999999,
+          },
+        },
+      }, 
+      {
+        version: "0.8.10"
+      }
+    ]
+    
+  },
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
+  },
+  etherscan: {
+    apiKey: "V5J7NIFIAWBVF81J6XFRRCR4ZH3SIC4DIW"
+  }
 };
+
 
 export default config;
