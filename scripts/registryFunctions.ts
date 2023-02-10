8b245258045ffe763360b612dfbb0bff57e32fa0
Jatin Jain <jatinj615@gmail.com>
2023-02-10 17:12:30 +0530

    feat: route deploy and add to registry

--- scripts/registryFunctions.ts
+++ scripts/registryFunctions.ts
@@ -1,21 +1,40 @@
-import { ethers } from "hardhat";
-import { connext } from './constants';
-import { Registry } from '../typechain/contracts/Registry';
-import { Contract } from 'ethers';
+// import { ethers } from "hardhat";
+// import { connext } from './constants';
+// import { Registry } from '../typechain/contracts/Registry';
+// import { Contract } from 'ethers';
+// import { Registry__factory } from '../typechain/factories/contracts/Registry__factory';
 
-// Management functions
-async function addRoute(routeData: Registry.RouteDataStruct[], registryContract: Contract) {
+// // Management functions
+// async function addRoute(routeAddress: string, registryAddress: string) {
 
-    const tx = await registryContract.addRoute(routeData);
+//     let routeData: Registry.RouteDataStruct[] = [{
+//         route: routeAddress,
+//         isEnabled: true
+//     }];
 
-}
+//     const [signer] = await ethers.getSigners();
 
-async function disableRoute(routeId: number, registryContract: Contract) {
-    const tx = await registryContract.disableRoute(routeId);
-}
+//     const registryFactory = new Registry__factory(signer);
+//     const registryContract = registryFactory.attach(registryAddress);
 
-async function addRegistryForDomain(remoteRegistries: Registry.RemoteRegistryStruct[], registryContract: Contract) {
-    const tx = await registryContract.addRemoteRegistry(remoteRegistries);
-}
+//     const tx = await registryContract.addRoute(routeData);
+    
+// }
 
-// User interfacing functions
+// async function disableRoute(routeId: number, registryContract: Contract) {
+//     const tx = await registryContract.disableRoute(routeId);
+// }
+
+// async function addRegistryForDomain(remoteRegistries: Registry.RemoteRegistryStruct[], registryContract: Contract) {
+//     const tx = await registryContract.addRemoteRegistry(remoteRegistries);
+// }
+
+// // User interfacing functions
+
+
+
+// export {
+//     addRoute,
+//     disableRoute,
+//     addRegistryForDomain
+// }
--- scripts/routeDeployment.ts
+++ scripts/routeDeployment.ts
@@ -1,43 +1,50 @@
-import { ethers } from "hardhat";
-import { registries } from './constants';
-import { verifyContract } from "./uitls";
+// import { ethers } from "hardhat";
+// import { registries } from './constants';
+// import { verifyContract } from "./uitls";
+// import { addRoute } from "./registryFunctions";
 
-async function deployRoute(registryAddress: string): Promise<string> {
+// async function deployRoute(registryAddress: string): Promise<string> {
 
-    const RouteFactory = await ethers.getContractFactory("AaveRoute");
+//     const RouteFactory = await ethers.getContractFactory("AaveRoute");
 
-    const route = await RouteFactory.deploy(registryAddress);
+//     const route = await RouteFactory.deploy(registryAddress);
 
-    const tx = await route.deployed();
+//     const tx = await route.deployed();
 
-    await tx.deployTransaction.wait(5);
+//     await tx.deployTransaction.wait(5);
 
-    console.log("route deployed")
+//     console.log("route deployed")
 
-    return route.address;
+//     console.log("adding route to registry");
 
-}
+//     await addRoute(route.address, registryAddress);
 
-async function main() {
-    const [signer] = await ethers.getSigners();
-    const network = await signer.provider?.getNetwork();
+//     console.log("route added to registry");
+    
+//     return route.address;
 
-    if (network?.chainId) {
-        console.log("deploying route..")
-        const routeAddress = await deployRoute(registries[network.chainId]);
-        console.log(routeAddress);
+// }
 
-        // verify Contract
-        await verifyContract(routeAddress, [registries[network.chainId]]);
+// async function main() {
+//     const [signer] = await ethers.getSigners();
+//     const network = await signer.provider?.getNetwork();
 
-    } else {
-        console.log("Network not found");
-    }
-}
+//     if (network?.chainId) {
+//         console.log("deploying route..")
+//         const routeAddress = await deployRoute(registries[network.chainId]);
+//         console.log(routeAddress);
 
-main()
-  .then(() => process.exit(0))
-  .catch((error: Error) => {
-    console.error(error);
-    process.exit(1);
-  });
+//         // verify Contract
+//         await verifyContract(routeAddress, [registries[network.chainId]]);
+
+//     } else {
+//         console.log("Network not found");
+//     }
+// }
+
+// main()
+//   .then(() => process.exit(0))
+//   .catch((error: Error) => {
+//     console.error(error);
+//     process.exit(1);
+//   });
